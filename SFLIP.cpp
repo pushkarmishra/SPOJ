1e9c1d128a6211f7c9bd528d74ee5f593bd4f24b
pushkarmishra <pushkarmishra@outlook.com>
2013-06-07 21:52:45 +0530

    Create SFLIP.cpp

---
+++ SFLIP.cpp
@@ -0,0 +1,12 @@
+/*
+Algo: Segment Tree
+Complexity: N*LogN
+*/
+
+/*
+The problem requires you to find K subsegments such that the sum is minimized.
+This can be done by finding the minimum sum subsegment and flipping it.
+Then again finding the minimum sum subsegment and flipping it.
+This has to be done K times.
+The total minimum sum is the sum of all the minimum sum subsegments found.
+*/
